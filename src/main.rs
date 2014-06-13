4b0bd1c3c549889611449b2e1d2db73948bf51d9
Will Long <will.long043@gmail.com>
2014-06-13 12:26:02 -0400

    Added basic command line input.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-// use std::io::BufferedReader;
+use std::io::BufferedReader;
 // use std::io::println;
-// use std::io;
+use std::io;
 mod token;
 mod mathparse;
 mod tokenize;
 
 fn main() {
-    // let mut reader = BufferedReader::new(io::stdin());
-    // let input = reader.read_line().unwrap();
-    // println!("Hello World!");
-    // println!("{}", &input);
-    //let program = "1 + 1\n";
-    let program = "(22 - ((-3) * 2))\n";
-    // let program = "(22 - (2 + 3) + 2)\n";
-    let all_tokens = tokenize::tokenize(program);
-    //println!("{}", all_tokens);
-    //println!("{}", mathparse::parse(all_tokens.as_slice()));
-    println!("{}", mathparse::parse(all_tokens.as_slice()));
+    let mut reader = BufferedReader::new(io::stdin());
+    while true {
+        let input = reader.read_line().unwrap();
+        // println!("Hello World!");
+        // println!("{}", &input);
+        // let program = "1 + 1\n";
+        // let program = "(22 - ((-3) * 2))\n";
+        // let program = "(22 - (2 + 3) + 2)\n";
+        let all_tokens = tokenize::tokenize(input.as_slice());
+        //println!("{}", all_tokens);
+        //println!("{}", mathparse::parse(all_tokens.as_slice()));
+        println!("{}", mathparse::parse(all_tokens.as_slice()));
+    }
 }
